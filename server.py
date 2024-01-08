0fdb8d13981674e183f19a34168c8e0873867639
Rashmi Padalkar <41661276+rashmi0405@users.noreply.github.com>
2024-01-08 16:13:43 -0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+import google.generativeai as palm
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+palm_api_key = os.environ["PALM_API_KEY"]
+palm.configure(api_key = palm_api_key)
+
+
+#Set up the Flask APP
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+
+@app.route("/chatbot" ,methods = ["POST"])
+
+def chatbot():
+    user_input = request.form["message"]
+    models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+    model = models[0].name
+    
+    prompt = f"User: {user_input} \n Sara The Bot: " 
+    
+    
+    #Generate the response
+    response = palm.generate_text(
+        model = model,
+        prompt = prompt,
+        stop_sequences = None,
+        temperature = 0,
+        max_output_tokens = 100) 
+    
+    bot_response = response.result
+    
+    
+    chat_history = []
+    chat_history.append(f"User: {user_input} \n Sara The Bot: {bot_response}")
+    
+    return render_template(
+        "chatbot.html",
+        user_input = user_input,
+        bot_response = bot_response,
+        chat_history = chat_history    
+    )
+    
+if __name__ == "__main__":
+    app.run(debug = True)
+    
+    
+    
+
